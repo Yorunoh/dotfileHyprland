58d2319f60f025ea76c47000ae88e37915e31526
Yoru <162798229+Yorunoh@users.noreply.github.com>
2026-08-21 12:35:26 +0700

    adjust from my old .conf file

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -16,10 +16,16 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
+    output   = "eDP-1",
+    mode     = "1920x1080@144",
     position = "auto",
-    scale    = "auto",
+    scale    = "1",
+})
+hl.monitor({ 
+    output = "HDMI-A-1",
+    mode = "preferred",
+    position = "auto",
+    scale = "auto",
 })
 
 
@@ -27,10 +33,11 @@ hl.monitor({
 ---- MY PROGRAMS ----
 ---------------------
 
--- Set programs that you use
+-- Set programs that you use   
 local terminal    = "kitty"
 local fileManager = "dolphin"
-local menu        = "hyprlauncher"
+-- local menu        = "hyprlauncher"
+local menu        = "rofi -show drun"
 
 
 -------------------
@@ -42,11 +49,12 @@ local menu        = "hyprlauncher"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function () 
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+ hl.on("hyprland.start", function ()
+   hl.exec_cmd(terminal)
+   hl.exec_cmd("nm-applet")
+   hl.exec_cmd("waybar & hyprpaper & hyprlock")
+   hl.exec_cmd("awww-daemon")
+ end)
 
 
 -------------------------------
